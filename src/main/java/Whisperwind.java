ca0c05ea5f49ff72e3e14593a98c8826496929fc
Noviana Ghovin <137738683+Novianaghovin@users.noreply.github.com>
2025-09-11 16:42:52 +0800

    Level-0

---
+++ src/main/java/Whisperwind.java
@@ -0,0 +1,8 @@
+public class Whisperwind {
+    public static void main(String[] args) {
+        String name = "Whisperwind";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
