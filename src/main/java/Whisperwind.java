2db4d4a28ef886cff06615bcdbda82374748af6c
Noviana Ghovin <137738683+Novianaghovin@users.noreply.github.com>
2025-09-11 17:04:27 +0800

    lLevel 1. Echo

--- src/main/java/Whisperwind.java
+++ src/main/java/Whisperwind.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Whisperwind {
     public static void main(String[] args) {
         String name = "Whisperwind";
         System.out.println("Hello! I'm " + name);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("What can I do for you?");
+        System.out.println("Type 'bye' to exit");
+        System.out.println("> ");
+        String Echo = scanner.nextLine();
+
+        while (!Echo.equalsIgnoreCase("bye")){
+            System.out.println(Echo);
+            System.out.println("> ");
+            Echo = scanner.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+
+        scanner.close();
     }
 }
